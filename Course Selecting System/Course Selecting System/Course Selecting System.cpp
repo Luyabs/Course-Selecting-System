0d3f56b8afff6083ddf62e6342b7f064cb08077e
canxuekratos <95893703+canxuekratos@users.noreply.github.com>
2022-01-06 22:41:12 +0800

    Update Course Selecting System.cpp

--- Course Selecting System/Course Selecting System/Course Selecting System.cpp
+++ Course Selecting System/Course Selecting System/Course Selecting System.cpp
@@ -1,6 +1,10 @@
 #include "Course Selecting System.h"
 using namespace std;
 
+const char* datafile1 = "stu.txt";
+const char* datafile2 = "course.txt";
+const char* datafile3 = "selection.txt";
+
 //!!! 在这里用switch-case写个功能菜单  考虑在打开功能菜单前需要自动用文件流输入自动加载数据
 
 int main()
@@ -12,6 +16,6 @@ int main()
 	LinkList<Course> Courses;
 	Courses.Load(datafile2);
 	Courses.Show();
-	cout << "请阅读Course Selecting System.h文件" << endl;
+	//cout << "请阅读Course Selecting System.h文件" << endl;
 	return 0;
 }
