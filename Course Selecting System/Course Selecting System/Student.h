b42aa8f23ab05edf4f400d97b564caf76c6f2e74
Ed-n1gma <73297782+Ed-n1gma@users.noreply.github.com>
2022-01-10 23:56:08 +0800

    Update Student.h

--- Course Selecting System/Course Selecting System/Student.h
+++ Course Selecting System/Course Selecting System/Student.h
@@ -6,15 +6,11 @@ struct Student
 {
 	int id;
 	string name;
+	int num;
 
 	Student();						// 无参数的构造函数
 	Student(int Id, string Name);		// 有参数的构造函数
-	string GetName() const; 			//返回姓名
-	int GetId() const;
-	
-	
 	friend ostream& operator <<(ostream& fout, const Student& s);
 	friend istream& operator >>(istream& fin, Student& s);
-	friend bool operator!=(Student a,Student b);
 };
 
