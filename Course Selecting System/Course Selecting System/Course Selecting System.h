e380d73224f40d10fed8bb2d1ce801db4b89bf69
Lu Yang <74538732+Luyabs@users.noreply.github.com>
2022-01-11 00:57:33 +0800

    Update

--- Course Selecting System/Course Selecting System/Course Selecting System.h
+++ Course Selecting System/Course Selecting System/Course Selecting System.h
@@ -16,22 +16,28 @@ public:
 	CSS();
 	~CSS() {};
 
-	
-	
+	//CSS
+	void Load();							//为CSS整合Student Course Selction
+	void Menu();							//为CSS整合功能菜单
+	void Menu_Admin();						//选课系统管理员菜单
+	void Menu_Student(int id);				//学生菜单
+	void Save();							//为CSS整合Save
+
+	//Student
 	void StuSave(const char* filename="stu.txt");  //保存学生名单到stu.txt的函数
-	void StuReset();  //从stu.txt读取数据的函数
+	void StuReset();								 //从stu.txt读取数据的函数
 	Status AddStu(int Id, string Name);				//增加学生
 	Status DeleteStu();                             //减少
 	
+	//Course
 	void CouSave(const char* filename = "course.txt");
 	void CouReset();
 
 	Status FindCou();							//查询课程被选情况
-
 	Status AddCou();
 	Status DeleteCou();
-
 	
+	//Selection
 	void SelSave(const char* filename = "selection.txt");//保存选择情况到selection.txt的函数
 	void SelLoad();//从selection.txt读取数据的函数
 };
