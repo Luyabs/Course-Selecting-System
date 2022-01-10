5b7b06d1719571606b88b214ef116dd443dfdf93
Ed-n1gma <73297782+Ed-n1gma@users.noreply.github.com>
2022-01-10 22:24:18 +0800

    构造函数完成

--- Course Selecting System/Course Selecting System/Course Selecting System.cpp
+++ Course Selecting System/Course Selecting System/Course Selecting System.cpp
@@ -5,7 +5,150 @@ CSS::CSS()
 {
 	Students.Load("stu.txt");
 	Courses.Load("course.txt");
+	CrossList<bool> t(Students.GetLength(), Courses.GetLength());
+	Selection = t;
 	Selection.Load("selection.txt");
+	for (int i = 1; i <= Students.GetLength(); i++)
+	{
+		Student s;
+		Students.GetElem(i,s);
+		int n = 0;
+		CrossNode<bool>* c;
+		for (c = Selection.rowHead[i - 1]; c != NULL; c = c->right)
+		{
+			Course C;
+			Courses.GetElem(c->triElem.col+1, C);
+			C.size++;
+			Courses.SetElem(c->triElem.col+1, C);
+			n++;
+		}
+		s.num = n;
+		Students.SetElem(i, s);
+	}
+	int id;
+	string Courseid;
+	cout << "请选择登入身份(1-900)：";
+	cin >> id;
+	if (id == 0)
+	{
+		char c = '*';
+		while (c != '0')
+		{
+			cout << std::left << setw(15) << "课程号"
+				<< setw(30) << "课程名" << setw(1) << "人数" << endl;
+			for (int i = 1; i <= Courses.GetLength(); i++)
+			{
+				Course C;
+				Courses.GetElem(i, C);
+				cout << std::left << setw(15) << C.id
+					<< setw(30) << C.name << setw(1) << C.size << '/' << C.maxsize << endl;
+			}
+			cout << endl << "1. 课程扩容.";
+			cout << endl << "2. 删除课程.";
+			cout << endl << "0. 退出";
+			cout << endl << "选择功能(0~2):";
+			cin >> c;
+			switch (c) {
+			case '1':
+				cout << "请输入课程号：";
+				cin >> Courseid;
+				for (int j = 1; j <= Courses.GetLength(); j++)
+				{
+					Course C;
+					Courses.GetElem(j, C);
+					if (C.id == Courseid)
+					{
+						int ex;
+						cout << "请输入要扩容的量：";
+						cin >> ex;
+						C.maxsize += ex;
+						Courses.SetElem(j, C);
+						break;
+					}
+				}
+				break;
+			case '2':
+				
+				break;
+			case '3':
+				
+				break;
+			}
+		}
+	}
+	else
+	{
+		Student s;
+		Students.GetElem(id, s);
+		cout << "您好，" << s.name << endl;
+		char c = '*';
+		while (c != '0')
+		{
+			cout << endl << "1. 查询已选课程.";
+			cout << endl << "2. 选课.";
+			cout << endl << "3. 删课.";
+			cout << endl << "0. 退出";
+			cout << endl << "选择功能(0~3):";
+			cin >> c;
+			switch (c) {
+			case '1':
+				CrossNode<bool> *cn;
+				cout << std::left << setw(15) << "课程号"
+					<< setw(30) << "课程名" << setw(1) << "学分" << endl;
+				for (cn = Selection.rowHead[id - 1]; cn != NULL; cn = cn->right)
+				{
+					Course C;
+					Courses.GetElem(cn->triElem.col+1, C);
+					cout << std::left << setw(15) << C.id
+						<< setw(30) << C.name << setw(1) << C.credits << endl;
+				}
+				break;
+			case '2':
+				if (s.num >= 5)
+					cout << "选课数目已达5门，不可再选！" << endl;
+				else
+				{
+					cout << std::left << setw(15) << "课程号"
+						<< setw(30) << "课程名" << setw(1) << "人数" << endl;
+					for (int i = 1; i <= Courses.GetLength(); i++)
+					{
+						Course C;
+						Courses.GetElem(i, C);
+						cout << std::left << setw(15) << C.id
+							<< setw(30) << C.name << setw(1) << C.size << '/' << C.maxsize << endl;
+					}
+					cout << "请输入课程号：";
+					cin >> Courseid;
+					for (int j = 1; j <= Courses.GetLength(); j++)
+					{
+						Course C;
+						Courses.GetElem(j, C);
+						if (C.id == Courseid)
+						{
+							if (C.size < C.maxsize)
+							{
+								Selection.SetElem(id - 1, j - 1, 1);
+							}
+							else
+							{
+								cout << "课程容量已满！" << endl;
+							}
+							break;
+						}
+						if(j==Courses.GetLength())
+							cout << "未找到课程号为" << Courseid << "的课程" << endl;
+					}
+				}
+				break;
+			case '3':
+				
+				break;
+			}
+		}
+	}
+	StuSave();
+	CouSave();
+	SelSave();
 }
 
 void CSS::StuSave(const char* filename)
