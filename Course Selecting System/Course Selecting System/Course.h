3ab1d3452881fca2f4f0c3c78fbda811a7537a51
Ed-n1gma <73297782+Ed-n1gma@users.noreply.github.com>
2022-01-10 23:55:03 +0800

    Update Course.h

--- Course Selecting System/Course Selecting System/Course.h
+++ Course Selecting System/Course Selecting System/Course.h
@@ -6,15 +6,13 @@ struct Course
 {
 	string id;
 	string name;
-	int credit;		//本课程学分
+	int	credits;		//本课程当前选课人数
 	int maxsize;	//本课程最大选课人数(容量)
+	int size;
 
 	Course();							// 无参数的构造函数
-	Course(string Id, string Name, int Credit, int Maxsize);	// 有参数的构造函数
-	string GetName() const; 			//返回课程名
-	int GetCredit() const;
-	int GetMaxsize() const;
-	friend bool operator!=(Course a,Course b);
+	Course(string Id, string Name, int Credits, int maxsize);	// 有参数的构造函数
+
 	friend ostream& operator <<(ostream& fout, const Course& c);
 	friend istream& operator >>(istream& fin, Course& c);
 };
